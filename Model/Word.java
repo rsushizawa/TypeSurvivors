0d9be521a405c680f8f1759d2057740b077b7601
rsushizaw <rodrigo.syu@puccampinas.edu.br>
2025-10-30 16:20:15 -0300

    feat: Change Typing Style to ZType Letters

--- Model/Word.java
+++ Model/Word.java
@@ -2,10 +2,12 @@ package Model;
 
 public class Word {
     public String text;
+    public final String originalText;
     public int x, y;
 
     Word(String text, int x, int y) {
         this.text = text;
+        this.originalText = text; 
         this.x = x;
         this.y = y;
     }
