7475bbbadd5c310e01fca4de3c0f56d555d4413c
Vinicius <viniciusmoraeschaves@gmail.com>
2025-12-10 12:45:16 -0300

    music addition

--- Config/PathsConfig.java
+++ Config/PathsConfig.java
@@ -20,8 +20,8 @@ public final class PathsConfig {
     // Projectile SFX files live in the Projectile subfolder
     public static final String PROJECTILE_SFX = SFX_DIR + "/Projectile/Projectile SFX1.wav";
     public static final String LOUVA_ATTACK_SFX = SFX_DIR + "/Louva Attack.wav";
-    public static final String MAIN_MENU_MUSIC = MUSIC_DIR + "/Main Menu Music.wav";
-    public static final String BOSS_MUSIC = MUSIC_DIR + "/Boss Music.wav";
+    public static final String MAIN_MENU_MUSIC = MUSIC_DIR + "/interludio.wav";
+    public static final String BOSS_MUSIC = MUSIC_DIR + "/StarWarsPart.wav";
     public static final String WRONG_KEY_SFX = SFX_DIR + "/WrongKey.wav";
     public static final String DEATH1_SFX = SFX_DIR + "/Death/death1.wav";
     public static final String DEATH2_SFX = SFX_DIR + "/Death/death2.wav";
